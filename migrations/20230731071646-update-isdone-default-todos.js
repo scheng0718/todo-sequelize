73cde82b2b9c438bc3308dfd92341e096f38a997
scheng0718 <scheng0718@gmail.com>
2023-07-31 00:37:03 -0700

    modify isdone default and add default seeder

---
+++ migrations/20230731071646-update-isdone-default-todos.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Todos', 'isDone', {
+      type: Sequelize.BOOLEAN,
+      allowNull: true,
+      defaultValue: false
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Todos', 'isDone', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    });
+  }
+};
