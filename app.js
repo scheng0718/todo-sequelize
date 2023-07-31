1f3ccc4748f3da5f7ab46e00b61d950b7693e89f
scheng0718 <scheng0718@gmail.com>
2023-07-31 14:12:06 -0700

    add isAuthenticated to display in different view

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
