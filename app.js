596c0d3c24e349b84e4fa0efa2d942a2e05d9bc2
scheng0718 <scheng0718@gmail.com>
2023-07-31 15:51:18 -0700

    add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const usePassport = require('./config/passport')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 const app = express()
 const PORT = 3000
@@ -15,9 +16,12 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(express.urlencoded({ extended: true }))
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -13,7 +13,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
@@ -22,10 +23,28 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       return res.render('register', {
+        error,
         name,
         email,
         password,
@@ -40,7 +59,10 @@ router.post('/register', (req, res) => {
         email,
         password: hash
       }))
-      .then(() => res.redirect('/'))
+      .then(() => {
+        req.flash('success_msg', '帳號建立，請登入')
+        res.redirect('/')
+      })
       .catch(err => console.log(err))
   })
 })
@@ -50,6 +72,7 @@ router.get('/logout', (req, res) => {
     if (err) {
       return next(err)
     }
+    req.flash('success_msg', '你已經成功登出。')
     res.redirect('/users/login')
   })
 })
